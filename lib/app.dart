eff132ae0de92512d98557d06a43e020f5dd9e25
frankylee <frankyleekelly@gmail.com>
2023-08-24 21:33:13 -0700

    LHB-40 Create bottom nav bar with ShellRoute branches
    This facilitates the persistent bottom navigation bar for the primary sections of the app (home, find, favorites, and settings).
    
    This sets up GoRouter with the nested navigation scaffold and shell route branches using the base pages in the previous patch. This demonstrates the potential of using subroutes and the capability of the bottom nav bar to remember the current route on each branch when switched tabs.
    
    <!-- ps-id: 7427259b-fe4d-46d8-9c57-d1e6c2d7907a -->

--- lib/app.dart
+++ lib/app.dart
@@ -1,51 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:littlehelpbook_flutter/common/extensions/build_context.ext.dart';
 
-class MyHomePage extends ConsumerStatefulWidget {
-  const MyHomePage({required this.title, super.key});
+// Stateful nested navigation based on:
+// https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
+// https://codewithandrea.com/articles/flutter-bottom-navigation-bar-nested-routes-gorouter
+class ScaffoldWithNestedNavigation extends StatelessWidget {
+  const ScaffoldWithNestedNavigation({
+    Key? key,
+    required this.navigationShell,
+  }) : super(key: key ?? const ValueKey('ScaffoldWithNestedNavigation'));
 
-  final String title;
-
-  @override
-  ConsumerState<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends ConsumerState<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() async {
-    setState(() {
-      _counter++;
-    });
-  }
+  final StatefulNavigationShell navigationShell;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: context.colorTheme.inversePrimary,
-        title: Text(widget.title),
+      body: navigationShell,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: navigationShell.currentIndex,
+        destinations: [
+          NavigationDestination(
+            label: context.l10n.home,
+            icon: Icon(Icons.home_outlined),
+          ),
+          NavigationDestination(
+            label: context.l10n.find,
+            icon: Icon(Icons.search),
+          ),
+          NavigationDestination(
+            label: context.l10n.favorites,
+            icon: Icon(Icons.favorite_border),
+          ),
+          NavigationDestination(
+            label: context.l10n.settings,
+            icon: Icon(Icons.settings_outlined),
+          ),
+        ],
+        onDestinationSelected: _goBranch,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              context.l10n.pushedTheButtonTimesLabel,
-            ),
-            Text(
-              '$_counter',
-              style: context.textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+
+  void _goBranch(int index) {
+    navigationShell.goBranch(
+      index,
+      // Support navigating to the initial location when tapping the item that is already active.
+      initialLocation: index == navigationShell.currentIndex,
     );
   }
 }
--- lib/common/router/lhb_router.dart
+++ lib/common/router/lhb_router.dart
@@ -2,8 +2,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:littlehelpbook_flutter/app.dart';
+import 'package:littlehelpbook_flutter/ui/favorite/favorites_screen.dart';
+import 'package:littlehelpbook_flutter/ui/find/find_screen.dart';
+import 'package:littlehelpbook_flutter/ui/home/home_screen.dart';
+import 'package:littlehelpbook_flutter/ui/settings/settings_screen.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKeyHome = GlobalKey<NavigatorState>(debugLabel: 'homeTab');
+final _shellNavigatorKeyFind = GlobalKey<NavigatorState>(debugLabel: 'findTab');
+final _shellNavigatorKeyFavorites =
+    GlobalKey<NavigatorState>(debugLabel: 'favoritesTab');
+final _shellNavigatorKeySettings =
+    GlobalKey<NavigatorState>(debugLabel: 'settingsTab');
 
 final lhbRouter = GoRouter(
   initialLocation: '/',
@@ -12,12 +22,65 @@ final lhbRouter = GoRouter(
   debugLogDiagnostics: kDebugMode,
 );
 
+// Stateful nested navigation based on:
+// https://github.com/flutter/packages/blob/main/packages/go_router/example/lib/stateful_shell_route.dart
+// https://codewithandrea.com/articles/flutter-bottom-navigation-bar-nested-routes-gorouter
 final _routes = [
-  GoRoute(
-    parentNavigatorKey: _rootNavigatorKey,
-    path: '/',
-    builder: (context, state) => const MyHomePage(
-      title: 'Little Help Book Home Page',
-    ),
+  StatefulShellRoute.indexedStack(
+    builder: (context, state, navigationShell) {
+      return ScaffoldWithNestedNavigation(navigationShell: navigationShell);
+    },
+    branches: [
+      StatefulShellBranch(
+        navigatorKey: _shellNavigatorKeyHome,
+        routes: [
+          GoRoute(
+            path: '/',
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: const MyHomePage(),
+            ),
+            routes: [
+              GoRoute(
+                path: 'lorem',
+                builder: (context, state) => const HomeSubroute(),
+              ),
+            ],
+          ),
+        ],
+      ),
+      StatefulShellBranch(
+        navigatorKey: _shellNavigatorKeyFind,
+        routes: [
+          GoRoute(
+            path: '/find',
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: const FindScreen(),
+            ),
+          ),
+        ],
+      ),
+      StatefulShellBranch(
+        navigatorKey: _shellNavigatorKeyFavorites,
+        routes: [
+          GoRoute(
+            path: '/favorites',
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: const FavoritesScreen(),
+            ),
+          ),
+        ],
+      ),
+      StatefulShellBranch(
+        navigatorKey: _shellNavigatorKeySettings,
+        routes: [
+          GoRoute(
+            path: '/settings',
+            pageBuilder: (context, state) => const NoTransitionPage(
+              child: const SettingsScreen(),
+            ),
+          ),
+        ],
+      ),
+    ],
   ),
 ];
