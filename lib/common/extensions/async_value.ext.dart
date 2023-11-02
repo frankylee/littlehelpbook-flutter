6e07e45f9550f5827b031d48491ac36ac4c2e85d
frankylee <frankyleekelly@gmail.com>
2023-11-02 16:04:35 -0700

    Add default error message for AsyncValue error snackbar
    This modifies the snackbar to take in an optional custom error message, ottherwise it will default to `Uh oh! Something is wrong. Please try again.`.
    
    <!-- ps-id: 305b8e8b-fdf7-4e9b-9ba6-0f0254412cb0 -->

--- lib/common/extensions/async_value.ext.dart
+++ lib/common/extensions/async_value.ext.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlehelpbook_flutter/common/extensions/build_context.ext.dart';
 
 extension AsyncValueExt on AsyncValue<dynamic> {
-  void showSnackbarOnError(BuildContext context) {
+  void showSnackbarOnError(BuildContext context, {String? message}) {
     if (!isLoading && hasError) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text(error.toString()),
+          content: Text(message ?? context.l10n.uhOhSomethingIsWrong),
         ),
       );
     }
