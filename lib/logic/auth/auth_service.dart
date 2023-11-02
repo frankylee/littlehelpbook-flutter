4cc49aee8e8fdedcbed84b5228ad812d397658cb
frankylee <frankyleekelly@gmail.com>
2023-11-02 16:08:26 -0700

    Add Auth Service and Provider
    This facilitates some of the basic Supabase auth methods that may be used.
    
    This has not been needed yet, but when needed this Service and Provider will define the interactions for managing auth. This currently contains some basic requests from the Supabase client.
    
    <!-- ps-id: 68f476bb-4550-4b77-9a11-07140d11737f -->

---
+++ lib/logic/auth/auth_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlehelpbook_flutter/data/supabase/supabase.dart';
+
+final authServiceProvider = Provider((ref) => AuthService());
+
+class AuthService {
+  AuthService();
+
+  bool isLoggedIn() {
+    return supabaseClient.auth.currentSession?.accessToken != null;
+  }
+
+  String? getUserId() {
+    return supabaseClient.auth.currentSession?.user.id;
+  }
+
+  Future<void> signOut() async {
+    return supabaseClient.auth.signOut();
+  }
+}
