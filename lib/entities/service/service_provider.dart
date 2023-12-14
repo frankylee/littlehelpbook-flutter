4d3ab9da70c503bfe832d1d7b0e56281e7e5a88c
frankylee <frankyleekelly@gmail.com>
2023-12-13 17:18:15 -0800

    Add services stream provider
    This facilitates listening to changes to the services datastore.
    
    This will replace the previous service future provider and the service client.
    
    <!-- ps-id: 27aceb78-2e52-4385-8671-483e00fa6590 -->

---
+++ lib/entities/service/service_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlehelpbook_flutter/shared/models/category.dart';
+import 'package:littlehelpbook_flutter/shared/models/service.dart';
+import 'package:littlehelpbook_flutter/shared/powersync/powersync.dart';
+
+final servicesStreamProvider = StreamProvider.family
+    .autoDispose<List<Service>, CategoryId?>((ref, categoryId) {
+  final query = categoryId != null
+      ? "SELECT * FROM services WHERE services.category_id = '$categoryId' ORDER BY name_en ASC"
+      : 'SELECT * FROM services ORDER BY name_en ASC';
+  return db.watch(query).map((res) {
+    return res.map(Service.fromMap).toList(growable: false);
+  });
+});
