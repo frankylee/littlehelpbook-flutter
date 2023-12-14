779cfc11f00e7c08d623ddfdc8f54d1c5b72d7dd
frankylee <frankyleekelly@gmail.com>
2023-12-13 17:11:25 -0800

    Add category and service models to shared directory
    This facilitates FSD by organizing shared models within the shared directory.
    
    This creates the category and service models in the `/shared/models` directory, which share some components, such as the id typedef. These models are the closest definition to the datastore. All other enhancements to the model, such as business logic, will be in the `/entities` directory.
    
    <!-- ps-id: 9a50dff1-b2f7-4930-b7a1-71a3b893a3c4 -->

---
+++ lib/shared/models/category.dart
@@ -0,0 +1,69 @@
+import 'package:equatable/equatable.dart';
+
+typedef CategoryId = String;
+
+class Category extends Equatable implements Comparable<Category> {
+  Category({
+    required this.id,
+    required this.nameEn,
+    this.nameEs,
+    required this.createdAt,
+    this.updatedAt,
+    this.deletedAt,
+  });
+
+  factory Category.fromMap(Map<String, dynamic> data) {
+    return Category(
+      id: data['id'] as CategoryId,
+      nameEn: data['name_en'] as String,
+      nameEs: data['name_es'] as String?,
+      createdAt: data['created_at'] as String,
+      updatedAt: data['updated_at'] as String?,
+      deletedAt: data['deleted_at'] as String?,
+    );
+  }
+
+  final CategoryId id;
+  final String nameEn;
+  final String? nameEs;
+  final String createdAt;
+  final String? updatedAt;
+  final String? deletedAt;
+
+  /// Sort categories by name in ascending order.
+  @override
+  int compareTo(Category other) {
+    return this.nameEn.compareTo(other.nameEn);
+  }
+
+  @override
+  List<Object?> get props => [
+        id,
+        nameEn,
+        nameEs,
+        createdAt,
+        updatedAt,
+        deletedAt,
+      ];
+
+  @override
+  bool? get stringify => true;
+
+  Category copyWith({
+    CategoryId? id,
+    String? nameEn,
+    String? nameEs,
+    String? createdAt,
+    String? updatedAt,
+    String? deletedAt,
+  }) {
+    return Category(
+      id: id ?? this.id,
+      nameEn: nameEn ?? this.nameEn,
+      nameEs: nameEs ?? this.nameEs,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      deletedAt: deletedAt ?? this.deletedAt,
+    );
+  }
+}
