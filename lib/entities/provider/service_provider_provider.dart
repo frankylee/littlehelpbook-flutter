65faf8ce5872ff668bb83a21bce644edea0a98d1
frankylee <frankyleekelly@gmail.com>
2023-12-15 20:58:16 -0800

    Add Service Provider provider stream and stream by Service Id
    This facilitates the retrieval of Providers from the datastore.
    
    This allows Providers to be retrieved by the associated Service or in its entirety.
    
    <!-- ps-id: 26ddb72f-9957-4abc-aee8-7c9942daa657 -->

---
+++ lib/entities/provider/service_provider_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlehelpbook_flutter/shared/models/provider.dart';
+import 'package:littlehelpbook_flutter/shared/models/service.dart';
+import 'package:littlehelpbook_flutter/shared/powersync/powersync.dart';
+
+final providersStreamProvider = StreamProvider<List<ServiceProvider>>((ref) {
+  return db
+      .watch("SELECT * FROM providers ORDER BY name ASC")
+      .map((res) => res.map(ServiceProvider.fromMap).toList(growable: false));
+});
+
+final providersByServiceProvider =
+    StreamProvider.family<List<ServiceProvider>, ServiceId>((ref, id) {
+  return db.watch(
+    "SELECT * FROM providers WHERE services LIKE '%?%' ORDER BY name ASC",
+    parameters: [id],
+  ).map((res) => res.map(ServiceProvider.fromMap).toList(growable: false));
+});
