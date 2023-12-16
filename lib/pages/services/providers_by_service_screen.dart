0a2e1829f71337d01b99beccbb8e4dbc11984646
frankylee <frankyleekelly@gmail.com>
2023-12-15 21:15:57 -0800

    Add Providers by Service Screen
    This displays a list of providers associated to a specific service.
    
    This will be a subroute of the Services > Services by Category > Providers by Service path.
    
    <!-- ps-id: 5adc044d-6109-4637-9409-849409221323 -->

---
+++ lib/pages/services/providers_by_service_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlehelpbook_flutter/entities/provider/service_provider_provider.dart';
+import 'package:littlehelpbook_flutter/entities/provider/widgets/providers_list.dart';
+import 'package:littlehelpbook_flutter/shared/extensions/async_value.ext.dart';
+import 'package:littlehelpbook_flutter/shared/models/provider.dart';
+import 'package:littlehelpbook_flutter/shared/models/service.dart';
+
+class ProvidersByServiceScreen extends ConsumerWidget {
+  const ProvidersByServiceScreen({
+    super.key,
+    required this.serviceId,
+    required this.serviceName,
+  });
+
+  final ServiceId serviceId;
+  final String serviceName;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<List<ServiceProvider>>>(
+      providersByServiceProvider(serviceId),
+      (_, state) => state.showSnackbarOnError(context),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(serviceName),
+      ),
+      body: ref.watch(providersByServiceProvider(serviceId)).maybeWhen(
+            data: (data) => ProvidersList(providers: data),
+            orElse: () => Center(child: CircularProgressIndicator()),
+          ),
+    );
+  }
+}
