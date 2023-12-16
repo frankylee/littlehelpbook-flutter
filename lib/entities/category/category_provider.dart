f3ca855d12fa8d3e91525a971bf3011b0a898fb6
frankylee <frankyleekelly@gmail.com>
2023-12-15 22:56:18 -0800

    Filter categories and services without children
    This cleans up the unnecessary list bloat for categories without services, or services without providers.
    
    This updates the category and service queries to use subqueries to filter out any entities that will not yield useful data to the user.
    
    <!-- ps-id: 74ea948b-e019-4dd0-bb17-1a3d2f4344fd -->

--- lib/entities/category/category_provider.dart
+++ lib/entities/category/category_provider.dart
@@ -3,7 +3,12 @@ import 'package:littlehelpbook_flutter/shared/models/category.dart';
 import 'package:littlehelpbook_flutter/shared/powersync/powersync.dart';
 
 final categoriesStreamProvider = StreamProvider<List<Category>>((ref) {
-  return db
-      .watch('SELECT * FROM categories ORDER BY name_en ASC')
-      .map((res) => res.map(Category.fromMap).toList(growable: false));
+  return db.watch(
+    '''SELECT * FROM categories WHERE EXISTS (
+      SELECT * FROM services WHERE EXISTS (
+        SELECT * FROM providers WHERE providers.services LIKE '%'||services.id||'%'
+      )
+      AND services.category_id = categories.id
+    ) ORDER BY name_en ASC''',
+  ).map((res) => res.map(Category.fromMap).toList(growable: false));
 });
--- lib/entities/service/service_provider.dart
+++ lib/entities/service/service_provider.dart
@@ -6,8 +6,13 @@ import 'package:littlehelpbook_flutter/shared/powersync/powersync.dart';
 final servicesStreamProvider =
     StreamProvider.family<List<Service>, CategoryId?>((ref, categoryId) {
   final query = categoryId != null
-      ? "SELECT * FROM services WHERE category_id = '$categoryId' ORDER BY name_en ASC"
-      : "SELECT * FROM services ORDER BY name_en ASC";
+      ? '''SELECT * FROM services WHERE EXISTS (
+          SELECT * FROM providers WHERE providers.services LIKE '%'||services.id||'%'
+        ) AND category_id = '${categoryId}'
+        ORDER BY name_en ASC'''
+      : '''SELECT * FROM services WHERE EXISTS (
+          SELECT * FROM providers WHERE providers.services LIKE '%'||services.id||'%'
+        ) ORDER BY name_en ASC''';
   return db.watch(query).map((res) {
     return res.map(Service.fromMap).toList(growable: false);
   });
