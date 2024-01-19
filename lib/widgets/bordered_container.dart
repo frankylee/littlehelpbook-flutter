d481fa390edb654358dc34bf676a3f82a13fb564
frankylee <frankyleekelly@gmail.com>
2024-01-19 15:08:35 -0800

    LHB-57 Add Bordered Container for location items
    This facilitates displaying each location in a rounded border rectangle on the Provider Details modal.
    
    This is a reusable component that takes in a child element and has a white border.
    
    <!-- ps-id: 7e520735-5806-4c29-9587-41ef1b940d90 -->

---
+++ lib/widgets/bordered_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class BorderedContainer extends ConsumerWidget {
+  const BorderedContainer({
+    super.key,
+    required this.child,
+    this.padding,
+  });
+
+  final Widget child;
+  final EdgeInsets? padding;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      width: double.maxFinite,
+      child: Padding(
+        padding: padding ??
+            EdgeInsets.symmetric(
+              vertical: 24.0,
+              horizontal: 16.0,
+            ),
+        child: child,
+      ),
+    );
+  }
+}
