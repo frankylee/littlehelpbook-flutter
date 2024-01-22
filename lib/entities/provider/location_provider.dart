2c5c7fd76a7143dfd6514ab264d0f64197b7c1df
chillbrodev <jon@uptechstudio.com>
2024-01-22 10:22:32 -0800

    Remove asBroadcastStream from locationStreamProvider
    This was not needed and was leftover from testing.

--- lib/entities/provider/location_provider.dart
+++ lib/entities/provider/location_provider.dart
@@ -6,8 +6,7 @@ import 'package:littlehelpbook_flutter/shared/powersync/powersync.dart';
 final locationsStreamProvider = StreamProvider<List<Location>>((ref) {
   return db
       .watch("SELECT * FROM locations ORDER BY name ASC")
-      .map((res) => res.map(Location.fromMap).toList(growable: false))
-      .asBroadcastStream();
+      .map((res) => res.map(Location.fromMap).toList(growable: false));
 });
 
 final locationByServiceProviderProvider =
