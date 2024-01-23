3549f607e820e1c3fe06b3488280f3c9b6d16138
chillbrodev <jon@uptechstudio.com>
2024-01-23 09:23:55 -0800

    Add User Location layer with permissions
    This adds the user's location onto the map IF they accept the
    permission.
    
    Plugin: https://github.com/tlserver/flutter_map_location_marker

--- lib/pages/find/find_screen.dart
+++ lib/pages/find/find_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
@@ -79,6 +80,7 @@ class FindScreenState extends ConsumerState<FindScreen>
               options: MarkerClusterLayerOptions(
                 maxClusterRadius: 45,
                 size: const Size(40, 40),
+                alignment: Alignment.center,
                 markers: ref.watch(validLocationsProvider).maybeWhen(
                       data: (locations) {
                         return locations
@@ -102,17 +104,20 @@ class FindScreenState extends ConsumerState<FindScreen>
                   return Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
+                      color: context.colorTheme.primaryContainer,
                     ),
                     child: Center(
                       child: Text(
                         markers.length.toString(),
-                        style: const TextStyle(color: Colors.white),
+                        style: context.textTheme.labelLarge,
                       ),
                     ),
                   );
                 },
               ),
+            ),
+            CurrentLocationLayer(
+              alignPositionOnUpdate: AlignOnUpdate.always,
             ),
             RichAttributionWidget(
               attributions: [
