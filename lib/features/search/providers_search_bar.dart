dbb538e60a886789124498db0a0f10c00c860102
frankylee <frankyleekelly@gmail.com>
2024-01-28 11:26:36 -0800

    LHB-83 Add Providers Search Bar feature
    This facilitates searching for providers by name or keyword in the description.
    
    This will be used by the Providers Screen so Sam can filter the list of providers to find what they are looking for. This includes localization for the search bar hint.
    
    <!-- ps-id: 4c63b0ff-9b71-4f5f-8c2d-88e177664868 -->

---
+++ lib/features/search/providers_search_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:littlehelpbook_flutter/shared/extensions/build_context.ext.dart';
+
+class ProviderSearchBar extends StatefulWidget {
+  const ProviderSearchBar({
+    super.key,
+    required this.searchNotifier,
+  });
+
+  final ValueNotifier<String> searchNotifier;
+
+  @override
+  State<StatefulWidget> createState() => _ProviderSearchBarState();
+}
+
+class _ProviderSearchBarState extends State<ProviderSearchBar> {
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      child: SearchBar(
+        controller: _textController,
+        elevation: MaterialStatePropertyAll(0.0),
+        hintText: context.l10n.searchProviders,
+        hintStyle: MaterialStatePropertyAll(
+          TextStyle(color: context.colorTheme.onSurface.withOpacity(0.75)),
+        ),
+        onChanged: (value) => widget.searchNotifier.value = value,
+        side: MaterialStatePropertyAll(
+          BorderSide(color: context.colorTheme.secondary),
+        ),
+        trailing: [
+          Icon(
+            Icons.search_rounded,
+            color: context.colorTheme.onSurface.withOpacity(0.75),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.close_rounded,
+              color: context.colorTheme.onSurface.withOpacity(0.75),
+            ),
+            onPressed: () {
+              setState(() {
+                _textController.text = '';
+                widget.searchNotifier.value = '';
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
