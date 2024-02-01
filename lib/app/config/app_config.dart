ea3f580946608c630fd89711528390e6413b0529
frankylee <frankyleekelly@gmail.com>
2024-01-31 18:50:28 -0800

    LHB-48 Add GrowthBook SDK and update AppConfig
    This facilitates toggling of features via GrowthBook.
    
    This adds the necessary package dependency and the AppConfig environment variables to the app.
    
    <!-- ps-id: 9f733868-7bc9-44ee-a478-5bf1ed43fdef -->

--- lib/app/config/app_config.dart
+++ lib/app/config/app_config.dart
@@ -4,6 +4,15 @@ part 'app_config.g.dart';
 
 @Envied()
 abstract class AppConfig {
+  @EnviedField(varName: 'GROWTHBOOK_API_KEY', obfuscate: true)
+  static final String growthBookApiKey = _AppConfig.growthBookApiKey;
+
+  @EnviedField(varName: 'GROWTHBOOK_HOST', obfuscate: true)
+  static final String growthBookHost = _AppConfig.growthBookHost;
+
+  @EnviedField(varName: 'MAPBOX_API_KEY', obfuscate: true)
+  static final String mapboxApiKey = _AppConfig.mapboxApiKey;
+
   @EnviedField(varName: 'POWERSYNC_DB_PATH', obfuscate: true)
   static final String powersyncDbPath = _AppConfig.powersyncDbPath;
 
@@ -12,7 +21,4 @@ abstract class AppConfig {
 
   @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
   static final String supabaseUrl = _AppConfig.supabaseUrl;
-
-  @EnviedField(varName: 'MAPBOX_API_KEY', obfuscate: true)
-  static final String mapboxApiKey = _AppConfig.mapboxApiKey;
 }
