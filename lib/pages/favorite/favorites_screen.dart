0d725c0f7460e359ccedd42c2bce8eaeaaf45c88
frankylee <frankyleekelly@gmail.com>
2024-02-01 20:36:22 -0800

    LHB-69 Display favorited providers on Favorites screen
    This facilitates a way to see all favorited providers and quickly access their info.
    
    This allows for the user to quickly unfavorite a provider. If there are more than 4 providers, a search bar will be displayed to easily filter the list.
    
    <!-- ps-id: 5c2caa6f-842e-4f2e-9734-9d8c29653879 -->

--- lib/pages/favorite/favorites_screen.dart
+++ lib/pages/favorite/favorites_screen.dart
@@ -1,37 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlehelpbook_flutter/entities/provider/widgets/providers_list.dart';
+import 'package:littlehelpbook_flutter/features/favorite/favorite_provider.dart';
+import 'package:littlehelpbook_flutter/features/search/providers_search_bar.dart';
+import 'package:littlehelpbook_flutter/shared/extensions/async_value.ext.dart';
 import 'package:littlehelpbook_flutter/shared/extensions/build_context.ext.dart';
-import 'package:littlehelpbook_flutter/shared/extensions/text_style.ext.dart';
-import 'package:littlehelpbook_flutter/widgets/button/primary_button.dart';
+import 'package:littlehelpbook_flutter/shared/models/provider.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends ConsumerWidget {
   const FavoritesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<List<ServiceProvider>>>(
+      favoritesProvider,
+      (_, state) => state.showSnackbarOnError(context),
+    );
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: context.colorTheme.inversePrimary,
+        centerTitle: true,
         title: Text(context.l10n.favorites),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              context.l10n.checkOutYourFavorites,
-              style: context.textTheme.headlineLarge?.secondary(context),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: ref.watch(favoritesProvider).maybeWhen(
+              data: FavoritesScreenDataView.new,
+              orElse: () => Center(child: CircularProgressIndicator()),
             ),
-            const SizedBox(height: 48.0),
-            PrimaryButton(
-              onPressed: () => context.go('404'),
-              text: '404 Page Not Found',
-            ),
-          ],
-        ),
       ),
+    );
+  }
+}
+
+class FavoritesScreenDataView extends StatefulWidget {
+  @visibleForTesting
+  const FavoritesScreenDataView(this.data, {super.key});
+
+  final List<ServiceProvider> data;
+
+  @override
+  State<StatefulWidget> createState() => _FavoritesScreenDataViewState();
+}
+
+class _FavoritesScreenDataViewState extends State<FavoritesScreenDataView> {
+  final _searchNotifier = ValueNotifier('');
+
+  @override
+  void dispose() {
+    _searchNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (widget.data.isNotEmpty && widget.data.length > 4)
+          ProviderSearchBar(searchNotifier: _searchNotifier),
+        const SizedBox(height: 16.0),
+        widget.data.isNotEmpty
+            ? ValueListenableBuilder(
+                valueListenable: _searchNotifier,
+                builder: (context, value, child) => ProvidersList(
+                  physics: NeverScrollableScrollPhysics(),
+                  providers: widget.data,
+                  searchTerm: value,
+                  shrinkWrap: true,
+                ),
+              )
+            : SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.5,
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 48.0),
+                    child: Text(
+                      context.l10n.entityCouldNotBeFound(
+                        context.l10n.noFavoritesYet,
+                      ),
+                      style: context.textTheme.titleMedium,
+                    ),
+                  ),
+                ),
+              ),
+      ],
     );
   }
 }
