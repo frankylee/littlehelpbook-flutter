0bd3b4e12e919b6989405383a9254f4f9cb450bb
frankylee <frankyleekelly@gmail.com>
2024-02-02 18:27:17 -0800

    LHB-57 Add Schedules by Location Provider
    This facilitates the retrieval of the schedules for a location from the database in order to be displayed on the Provider Details bottom modal sheet.
    
    <!-- ps-id: f2738773-c1b5-43be-ac59-0f234521cf08 -->

--- lib/entities/provider/location_provider.dart
+++ lib/entities/provider/location_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:littlehelpbook_flutter/shared/models/location.dart';
 import 'package:littlehelpbook_flutter/shared/models/provider.dart';
+import 'package:littlehelpbook_flutter/shared/models/schedule.dart';
 import 'package:littlehelpbook_flutter/shared/powersync/powersync.dart';
 
 final locationsStreamProvider = StreamProvider<List<Location>>((ref) {
@@ -14,4 +15,18 @@ final locationByServiceProviderProvider =
   return db.watch(
     '''SELECT * FROM locations WHERE provider_id = '${id}' ORDER BY name ASC''',
   ).map((res) => res.map(Location.fromMap).toList(growable: false));
+});
+
+final schedulesByLocationProvider =
+    StreamProvider.family<List<Schedule>, LocationId>((ref, id) {
+  return db
+      .watch("SELECT * FROM schedules WHERE location_id = '${id}'")
+      .map((res) {
+    final schedules = res
+        .map(Schedule.fromMap)
+        .where((e) => e.isActive())
+        .toList(growable: false);
+    schedules.sort();
+    return schedules;
+  });
 });
