9eedfdaff1295ec8f6c8285b0ace3889389cb0dc
frankylee <frankyleekelly@gmail.com>
2024-02-03 23:18:28 -0800

    LHB-58 Wrap MaterialApp in ResponsiveApp
    This facilitates responsiveness across mobile and tablet.
    
    This is necessary to use features from the `responsive_builder` package, such as responsive sizing utilities.
    
    <!-- ps-id: 164dd063-38b0-4d5e-bac9-64b1b1ca7c9a -->

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:littlehelpbook_flutter/app/router/lhb_router.dart';
 import 'package:littlehelpbook_flutter/app/theme/lhb_theme.dart';
 import 'package:littlehelpbook_flutter/generated/l10n.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class LittleHelpBook extends StatelessWidget {
   const LittleHelpBook({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Little Help Book',
-      theme: LittleHelpBookTheme.lightTheme,
-      darkTheme: LittleHelpBookTheme.darkTheme,
-      localizationsDelegates: const [S.delegate],
-      supportedLocales: S.delegate.supportedLocales,
-      routerConfig: lhbRouter,
+    return ResponsiveApp(
+      builder: (context) => MaterialApp.router(
+        title: 'Little Help Book',
+        theme: LittleHelpBookTheme.lightTheme,
+        darkTheme: LittleHelpBookTheme.darkTheme,
+        localizationsDelegates: const [S.delegate],
+        supportedLocales: S.delegate.supportedLocales,
+        routerConfig: lhbRouter,
+      ),
     );
   }
 }
