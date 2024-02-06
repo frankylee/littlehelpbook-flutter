055ebf16a6b0e48673704f73e2e978beadd04cc1
frankylee <frankyleekelly@gmail.com>
2024-02-05 20:19:44 -0800

    LHB-49 Add `package_info_plus` to dependencies
    This facilitates getting the current app version and evaluating against the GrowthBook toggles for app versions.
    
    This is necessary to display a hard version update or soft version modal to the user.
    
    <!-- ps-id: 7937679a-b180-4647-a071-e5e0899a77cf -->

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import app_links
 import geolocator_apple
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqlite3_flutter_libs
@@ -15,6 +16,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
