908cc9f64902e26e32aff634f2715df27f56241e
frankylee <frankyleekelly@gmail.com>
2024-02-09 19:39:25 -0800

    LHB-107 Display the current app version on the Settings Screen
    This lets users know which version of the app they are on and can assist with debugging.
    
    This will be useful when we implement bug reporting.
    
    <!-- ps-id: 916df3a8-f03f-47e7-8bc1-3f5a329b1b29 -->

--- lib/pages/settings/settings_screen.dart
+++ lib/pages/settings/settings_screen.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:littlehelpbook_flutter/features/live_query/query_widget.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:littlehelpbook_flutter/shared/app_version/app_version_provider.dart';
 import 'package:littlehelpbook_flutter/shared/extensions/build_context.ext.dart';
+import 'package:littlehelpbook_flutter/shared/extensions/text_style.ext.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends ConsumerWidget {
   const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: context.colorTheme.inversePrimary,
         title: Text(context.l10n.settings),
       ),
-      body: QueryWidget(defaultQuery: 'SELECT * from categories LIMIT 10'),
-      // body: Padding(
-      //   padding: EdgeInsets.symmetric(horizontal: 24.0),
-      //   child: Column(
-      //     crossAxisAlignment: CrossAxisAlignment.start,
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       Text(
-      //         context.l10n.adjustYourSettings,
-      //         style: context.textTheme.headlineLarge?.primary(context),
-      //       ),
-      //     ],
-      //   ),
-      // ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              context.l10n.adjustYourSettings,
+              style: context.textTheme.headlineLarge?.primary(context),
+            ),
+            const SizedBox(height: 48.0),
+            ref.watch(appVersionProvider).maybeWhen(
+                  data: (version) => Center(
+                    child: Text(
+                      context.l10n.appVersion(version),
+                      style: context.textTheme.bodySmall,
+                    ),
+                  ),
+                  orElse: () => const SizedBox.shrink(),
+                ),
+          ],
+        ),
+      ),
     );
   }
 }
