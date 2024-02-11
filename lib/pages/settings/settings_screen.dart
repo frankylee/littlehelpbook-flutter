84b50d3f9d8fb0572b04f369c4d9b12cdcc60d3b
frankylee <frankyleekelly@gmail.com>
2024-02-11 14:59:02 -0800

    LHB-58 Add responsiveness to Settings Screen
    This facilitates a responsive layout on the landscape mode or on a tablet.
    
    This adds a max width to the content to be within the safe area of the device.
    
    <!-- ps-id: e437e0da-1c8c-47f6-89ca-b67d3c74c066 -->

--- lib/pages/settings/settings_screen.dart
+++ lib/pages/settings/settings_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:littlehelpbook_flutter/app/theme/lhb_style_constants.dart';
 import 'package:littlehelpbook_flutter/features/toggle/l10n_toggle.dart';
 import 'package:littlehelpbook_flutter/features/toggle/toggle_theme.dart';
 import 'package:littlehelpbook_flutter/shared/extensions/build_context.ext.dart';
-import 'package:littlehelpbook_flutter/shared/extensions/text_style.ext.dart';
 import 'package:littlehelpbook_flutter/widgets/app_version.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -14,23 +14,30 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(context.l10n.settings),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              context.l10n.adjustYourSettings,
-              style: context.textTheme.headlineLarge?.primary(context),
+      body: SingleChildScrollView(
+        padding: LhbStyleConstants.pagePaddingInsets,
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: LhbStyleConstants.maxPageContentWidth,
             ),
-            const SizedBox(height: 48.0),
-            ToggleTheme(),
-            const SizedBox(height: 24.0),
-            L10nToggle(),
-            const SizedBox(height: 48.0),
-            AppVersion(),
-          ],
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  context.l10n.adjustYourSettings,
+                  style: context.textTheme.bodyLarge,
+                ),
+                const SizedBox(height: 48.0),
+                ToggleTheme(),
+                const SizedBox(height: 24.0),
+                L10nToggle(),
+                const SizedBox(height: 48.0),
+                AppVersion(),
+              ],
+            ),
+          ),
         ),
       ),
     );
