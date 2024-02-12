adb6b9876d6aa516ccc8445e43d05260bf17135b
frankylee <frankyleekelly@gmail.com>
2024-02-11 20:08:40 -0800

    LHB-4 Add Sentry DSN to Secrets
    This facilitates retrieving the Sentry DSN secret from the Doppler CLI and using the config within the application.
    
    <!-- ps-id: 1f4a59f8-fdfb-4cfc-852e-807578a10365 -->

--- lib/app/config/app_config.dart
+++ lib/app/config/app_config.dart
@@ -4,6 +4,9 @@ part 'app_config.g.dart';
 
 @Envied()
 abstract class AppConfig {
+  @EnviedField(varName: 'FLAVOR')
+  static final String flavor = _AppConfig.flavor;
+
   @EnviedField(varName: 'GROWTHBOOK_API_KEY', obfuscate: true)
   static final String growthBookApiKey = _AppConfig.growthBookApiKey;
 
@@ -15,6 +18,9 @@ abstract class AppConfig {
 
   @EnviedField(varName: 'POWERSYNC_DB_PATH', obfuscate: true)
   static final String powersyncDbPath = _AppConfig.powersyncDbPath;
+
+  @EnviedField(varName: 'SENTRY_DSN', obfuscate: true)
+  static final String sentryDsn = _AppConfig.sentryDsn;
 
   @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
   static final String supabaseAnonKey = _AppConfig.supabaseAnonKey;
