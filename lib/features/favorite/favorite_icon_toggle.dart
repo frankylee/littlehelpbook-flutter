8e4190a562fe5f5bd2c27ad31086d5b1d1eb474b
frankylee <frankyleekelly@gmail.com>
2025-02-01 18:55:21 -0800

    feat: color the favorite heart icon with tertiary palette
    This adds a pop of color to the favorite icon.
    
    <!-- ps-id: f5da8c2f-27d6-47e1-9a45-7d1e7153336c -->

--- lib/features/favorite/favorite_icon_toggle.dart
+++ lib/features/favorite/favorite_icon_toggle.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:littlehelpbook_flutter/features/favorite/favorite_provider.dart';
+import 'package:littlehelpbook_flutter/shared/extensions/build_context.ext.dart';
 import 'package:littlehelpbook_flutter/shared/models/provider.dart';
 
 class FavoriteIconToggle extends ConsumerWidget {
@@ -17,7 +18,10 @@ class FavoriteIconToggle extends ConsumerWidget {
     final isFavorite =
         ref.read(favoritesProvider.notifier).isFavorite(providerId);
     return GestureDetector(
-      child: Icon(isFavorite ? Icons.favorite : Icons.favorite_border_rounded),
+      child: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border_rounded,
+        color: context.colorTheme.tertiary,
+      ),
       onTap: () async {
         ref.read(favoritesProvider.notifier).toggleFavorite(providerId);
       },
